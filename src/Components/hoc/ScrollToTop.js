d4c376867f445d878fc271769ea4a6d350b0890c
dev-seack <seack@ts-dm.de>
2019-09-29 14:29:14 +0200

    fixed Aux Compnent Bug; Created .gitignore

--- src/Components/hoc/ScrollToTop.js
+++ src/Components/hoc/ScrollToTop.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
-import { useRouter } from 'state';
+import { useEffect } from "react";
+import { useRouter } from "state";
 
 // Component that attaches scroll to top hanler on router change
 // renders nothing, just attaches side effects
 export const ScrollToTopControlller = () => {
   // this assumes that current router state is accessed via hook
   // but it does not matter, pathname and search (or that ever) may come from props, context, etc.
-  const  { pathname, search } = useRouter();
-  
+  const { pathname, search } = useRouter();
+
   // just run the effect on pathname and/or search change
   useEffect(() => {
     try {
@@ -15,14 +15,14 @@ export const ScrollToTopControlller = () => {
       window.scroll({
         top: 0,
         left: 0,
-        behavior: 'smooth',
+        behavior: "smooth"
       });
     } catch (error) {
       // just a fallback for older browsers
       window.scrollTo(0, 0);
     }
   }, [pathname, search]);
-  
+
   // renders nothing, since nothing is needed
   return null;
 };
