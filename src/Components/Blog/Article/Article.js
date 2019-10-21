00338f5e9fd26587a09b92e84d3f9e5b49e4f324
dev-seack <ms@legarti.de>
2019-10-21 19:20:47 +0200

    Blogartikel: maxContainer hinzugefuegt

--- src/Components/Blog/Article/Article.js
+++ src/Components/Blog/Article/Article.js
@@ -20,82 +20,93 @@ const Article = () => {
           hier entsteht der neue webauftritt von ucs
         </SectionHeadline>
       </div>
-      <Aux isContentContainer>
-        <p className="AuthorName">Auhtor: Elisabeth Lopez</p>
-        <p className="Created">05. Mai 2019</p>
-      </Aux>
-      <SectionHeadline headline="Die neue Webseite kommt" />
-      <Aux isContentContainer>
-        <p className="ArticleText">
-          Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen, extern sowie
-          Intern teil. Ausserdem teilen wir gerne Trainingseinheiten,
-          Wettkaempfe, Veranstalltungen und viele weitere spannende Dinge rund
-          um und ueber den United Cheer Sports Verein in Dortmund.Wir nehmen
-          regelmaessig an Wettkaempfen, Vorstellungen, extern sowie Intern teil.
-          Ausserdem teilen wir gerne Trainingseinheiten, Wettkaempfe,
-          Veranstalltungen und viele weitere spannende Dinge rund um und ueber
-          den United Cheer Sports Verein in Dortmund.Wir nehmen regelmaessig an
-          Wettkaempfen, Vorstellungen, extern sowie Intern teil. Ausserdem
-          teilen wir gerne Trainingseinheiten, Wettkaempfe, Veranstalltungen und
-          viele weitere spannende Dinge rund um und ueber den United Cheer
-          Sports Verein in Dortmund.Wir nehmen regelmaessig an Wettkaempfen,
-          Vorstellungen, extern sowie Intern teil. Ausserdem teilen wir gerne
-          Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
-          spannende Dinge rund um und ueber den United Cheer Sports Verein in
-          Dortmund.Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen,
-          extern sowie Intern teil. Ausserdem teilen wir gerne
-          Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
-          spannende Dinge rund um und ueber den United Cheer Sports Verein in
-          Dortmund.Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen,
-          extern sowie Intern teil. Ausserdem teilen wir gerne
-          Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
-          spannende Dinge rund um und ueber den United Cheer Sports Verein in
-          Dortmund.Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen,
-          extern sowie Intern teil. Ausserdem teilen wir gerne
-          Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
-          spannende Dinge rund um und ueber den United Cheer Sports Verein in
-          Dortmund.Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen,
-          extern sowie Intern teil. Ausserdem teilen wir gerne
-          Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
-          spannende Dinge rund um und ueber den United Cheer Sports Verein in
-          Dortmund.
-        </p>
-        <div className="ImpressionContainer">
-          <img src={ArticleImage} alt="Alternative Text" />
-          <img src={ArticleImage} alt="Alternative Text" />
-          <img src={ArticleImage} alt="Alternative Text" />
-        </div>
-        <SectionHeadline headline="Weitere News von UCS" />
-        <div className="ArticleNewsInnerContainer">
-          <Link to={"/article/" + articlesID} className="NewsPreviewContainer">
-            <NewsPreview
-              headline="Silver Spirit Dancer"
-              imgurl="http://via.placeholder.com/300x300"
+      <div className="maxContainer">
+        <Aux isContentContainer>
+          <p className="AuthorName">Auhtor: Elisabeth Lopez</p>
+          <p className="Created">05. Mai 2019</p>
+        </Aux>
+        <SectionHeadline headline="Die neue Webseite kommt" />
+        <Aux isContentContainer>
+          <p className="ArticleText">
+            Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen, extern sowie
+            Intern teil. Ausserdem teilen wir gerne Trainingseinheiten,
+            Wettkaempfe, Veranstalltungen und viele weitere spannende Dinge rund
+            um und ueber den United Cheer Sports Verein in Dortmund.Wir nehmen
+            regelmaessig an Wettkaempfen, Vorstellungen, extern sowie Intern
+            teil. Ausserdem teilen wir gerne Trainingseinheiten, Wettkaempfe,
+            Veranstalltungen und viele weitere spannende Dinge rund um und ueber
+            den United Cheer Sports Verein in Dortmund.Wir nehmen regelmaessig
+            an Wettkaempfen, Vorstellungen, extern sowie Intern teil. Ausserdem
+            teilen wir gerne Trainingseinheiten, Wettkaempfe, Veranstalltungen
+            und viele weitere spannende Dinge rund um und ueber den United Cheer
+            Sports Verein in Dortmund.Wir nehmen regelmaessig an Wettkaempfen,
+            Vorstellungen, extern sowie Intern teil. Ausserdem teilen wir gerne
+            Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
+            spannende Dinge rund um und ueber den United Cheer Sports Verein in
+            Dortmund.Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen,
+            extern sowie Intern teil. Ausserdem teilen wir gerne
+            Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
+            spannende Dinge rund um und ueber den United Cheer Sports Verein in
+            Dortmund.Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen,
+            extern sowie Intern teil. Ausserdem teilen wir gerne
+            Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
+            spannende Dinge rund um und ueber den United Cheer Sports Verein in
+            Dortmund.Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen,
+            extern sowie Intern teil. Ausserdem teilen wir gerne
+            Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
+            spannende Dinge rund um und ueber den United Cheer Sports Verein in
+            Dortmund.Wir nehmen regelmaessig an Wettkaempfen, Vorstellungen,
+            extern sowie Intern teil. Ausserdem teilen wir gerne
+            Trainingseinheiten, Wettkaempfe, Veranstalltungen und viele weitere
+            spannende Dinge rund um und ueber den United Cheer Sports Verein in
+            Dortmund.
+          </p>
+          <div className="ImpressionContainer">
+            <img src={ArticleImage} alt="Alternative Text" />
+            <img src={ArticleImage} alt="Alternative Text" />
+            <img src={ArticleImage} alt="Alternative Text" />
+          </div>
+          <SectionHeadline headline="Weitere News von UCS" />
+          <div className="ArticleNewsInnerContainer">
+            <Link
+              to={"/article/" + articlesID}
+              className="NewsPreviewContainer"
             >
-              Unsere Dancer waren sehr erfolgreich bei der deutschen
-              Meisterschaft
-            </NewsPreview>
-          </Link>
-          <Link to={"/article/" + articlesID} className="NewsPreviewContainer">
-            <NewsPreview
-              headline="Silver Spirit Dancer"
-              imgurl="http://via.placeholder.com/300x300"
+              <NewsPreview
+                headline="Silver Spirit Dancer"
+                imgurl="http://via.placeholder.com/300x300"
+              >
+                Unsere Dancer waren sehr erfolgreich bei der deutschen
+                Meisterschaft
+              </NewsPreview>
+            </Link>
+            <Link
+              to={"/article/" + articlesID}
+              className="NewsPreviewContainer"
             >
-              Unsere Dancer waren sehr erfolgreich bei der deutschen
-              Meisterschaft
-            </NewsPreview>
-          </Link>
-          <Link to={"/article/" + articlesID} className="NewsPreviewContainer">
-            <NewsPreview
-              headline="Silver Spirit Dancer"
-              imgurl="http://via.placeholder.com/300x300"
+              <NewsPreview
+                headline="Silver Spirit Dancer"
+                imgurl="http://via.placeholder.com/300x300"
+              >
+                Unsere Dancer waren sehr erfolgreich bei der deutschen
+                Meisterschaft
+              </NewsPreview>
+            </Link>
+            <Link
+              to={"/article/" + articlesID}
+              className="NewsPreviewContainer"
             >
-              Unsere Dancer waren sehr erfolgreich bei der deutschen
-              Meisterschaft
-            </NewsPreview>
-          </Link>
-        </div>
-      </Aux>
+              <NewsPreview
+                headline="Silver Spirit Dancer"
+                imgurl="http://via.placeholder.com/300x300"
+              >
+                Unsere Dancer waren sehr erfolgreich bei der deutschen
+                Meisterschaft
+              </NewsPreview>
+            </Link>
+          </div>
+        </Aux>
+      </div>
     </Enc>
   );
 };
