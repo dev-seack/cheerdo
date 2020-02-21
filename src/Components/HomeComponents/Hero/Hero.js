d396ccbe12cc281f489ea011a9cfa05badb50f76
itreptau <igor@treptau.eu>
2020-02-21 19:24:56 +0100

    removed react-player;

--- src/Components/HomeComponents/Hero/Hero.js
+++ src/Components/HomeComponents/Hero/Hero.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Hero.css";
 import video from "../../../assets/homeVideo.mp4";
-import ReactPlayer from 'react-player'
 import poster from "../../../assets/homeVideo-poster.jpeg";
 import headline from "../../../assets/united-cheer-sports-schriftzug.svg";
 import instagramLogo from "../../../assets/united-cheer-sports-instagram.svg";
@@ -9,60 +8,62 @@ import facebookLogo from "../../../assets/united-cheer-sports-facebook.svg";
 
 import { PrimaryButton } from "../../Buttons/PrimaryButton";
 
-const Hero = props => {
-  return (
-    <div className="HeroContainer">
-      <ReactPlayer
-        url={video}
-        className='VideoBackground'
-        width='auto' height='auto'
-        playing loop muted volume="1"
-        config={{
-          file: {
-            attributes: {
-              poster: {poster}
-            }
-          }
-        }}
-      />
-      <div className="InnerHeroContainer">
-        <img
-          className="HeroHeadline"
-          src={headline}
-          alt="United Cheer Sports e.V. Dortmund Schriftzug, Cheeleading, Cheerdance, Sport in Dortmund"
-        />
-        <p className="HeroSubline">
-          Chheerleading und Cheerdance sind eigenst&auml;ndige
-          Wettkampfsportarten und vereinen Turnen, Akrobatik und Tanz mit
-          verschiedenen Schwerpunkten.
-        </p>
-        <p className="allTogether">Black White Green</p>
-        <PrimaryButton text="werde teil von ucs" to="probetraining" />
-      </div>
-      <div className="HeroSocialContainer">
-        <a
-          href="https://www.instagram.com/unitedcheersports_cheerleader"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+class Hero extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVideoMuted: true
+    };
+  }
+
+  render() {
+    return (
+      <div className="HeroContainer">
+        <div className="VideoBackground">
+          <video loop muted={this.state.isVideoMuted} autoPlay poster={poster}>
+            <source src={video} type="video/mp4" />
+            Dein Browser unterstuetzt keine Videos
+          </video>
+        </div>
+        <div className="InnerHeroContainer">
           <img
-            src={instagramLogo}
+            className="HeroHeadline"
+            src={headline}
             alt="United Cheer Sports e.V. Dortmund Schriftzug, Cheeleading, Cheerdance, Sport in Dortmund"
           />
-        </a>
-        <a
-          href="https://www.facebook.com/UnitedCheerSports/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={facebookLogo}
-            alt="United Cheer Sports e.V. Dortmund Schriftzug, Cheeleading, Cheerdance, Sport in Dortmund"
-          />
-        </a>
+          <p className="HeroSubline">
+            Chheerleading und Cheerdance sind eigenst&auml;ndige
+            Wettkampfsportarten und vereinen Turnen, Akrobatik und Tanz mit
+            verschiedenen Schwerpunkten.
+          </p>
+          <p className="allTogether">Black White Green</p>
+          <PrimaryButton text="werde teil von ucs" to="probetraining" />
+        </div>
+        <div className="HeroSocialContainer">
+          <a
+            href="https://www.instagram.com/unitedcheersports_cheerleader"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={instagramLogo}
+              alt="United Cheer Sports e.V. Dortmund Schriftzug, Cheeleading, Cheerdance, Sport in Dortmund"
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/UnitedCheerSports/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={facebookLogo}
+              alt="United Cheer Sports e.V. Dortmund Schriftzug, Cheeleading, Cheerdance, Sport in Dortmund"
+            />
+          </a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export { Hero };
